d45b5c2e6014f9a0c5d1fb30a0e7f00e24df001d
May Miller-Ricci <maymillerricci@gmail.com>
2014-01-20 13:28:46 -0800

    make a basic user model

--- config/application.rb
+++ config/application.rb
@@ -7,12 +7,14 @@ require "action_mailer/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
 module SampleApp
   class Application < Rails::Application
+    config.i18n.enforce_available_locales = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

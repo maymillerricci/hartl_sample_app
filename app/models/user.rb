54691d25c12475de8e0e29516fa1ac203f8a96a3
May Miller-Ricci <maymillerricci@gmail.com>
2014-01-20 14:04:33 -0800

    update to user model for rails 4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,17 @@
 class User < ActiveRecord::Base
 
-	has_secure_password
-
-	before_save { |user| user.email = user.email.downcase }
+	# before_save { |user| user.email = user.email.downcase }
+	before_save { self.email = email.downcase }
 
 	validates :name,  presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
-						uniqueness: { case_sensitive: false }
+	validates :email, presence: true, 
+										format: { with: VALID_EMAIL_REGEX }, 
+										uniqueness: { case_sensitive: false }
+	
+	has_secure_password
+	
+	# no longer need to validate presence of password & password_confirmation?
 	validates :password, presence: true, length: { minimum: 6 }
 	validates :password_confirmation, presence: true
 end

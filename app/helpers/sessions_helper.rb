d6ae2038787fdc6c9813c922c8142c29e5816fb6
May Miller-Ricci <maymillerricci@gmail.com>
2014-01-27 00:53:07 -0800

    update to token encryption

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,9 @@
 module SessionsHelper
 	
 	def sign_in(user)
-		cookies.permanent[:remember_token] = user.remember_token
+		remember_token = User.new_remember_token
+		cookies.permanent[:remember_token] = remember_token
+		user.update_attribute(:remember_token, User.encrypt(remember_token))
 		self.current_user = user
 	end
 
@@ -14,13 +16,16 @@ module SessionsHelper
 	end
 
 	def current_user
+		remember_token = User.encrypt(cookies[:remember_token])
 		# @current_user = @current_user || User.find_by_remember_token(cookies[:remember_token])
-		@current_user ||= User.find_by_remember_token(cookies[:remember_token])
+		@current_user ||= User.find_by(remember_token: remember_token)
 	end
 
 	def sign_out
-		self.current_user = nil
+		current_user.update_attribute(:remember_token,
+																	User.encrypt(User.new_remember_token))
 		cookies.delete(:remember_token)
+		self.current_user = nil
 	end
 
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -31,7 +31,7 @@ describe "AuthenticationPages" do
   	describe "with valid information" do
   		let(:user) { FactoryGirl.create(:user) }
   		before do
-  			fill_in "Email",    with: user.email
+  			fill_in "Email",    with: user.email.upcase
   			fill_in "Password", with: user.password
   			click_button "Sign in"
   		end

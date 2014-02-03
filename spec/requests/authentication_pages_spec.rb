de000a118347eaad4246d4acb2a627308186fc23
May Miller-Ricci <maymillerricci@gmail.com>
2014-02-02 17:32:14 -0800

    add users page

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "AuthenticationPages" do
+describe "Authentication" do
 
 	subject { page }
   
@@ -34,7 +34,8 @@ describe "AuthenticationPages" do
 
   		it { should have_title(user.name) }
   		it { should have_link('Profile',     href: user_path(user)) }
-  		it { should have_link('Settings',    href: edit_user_path(user)) }
+      it { should have_link('Settings',    href: edit_user_path(user)) }
+  		it { should have_link('Users',       href: users_path) }
       it { should have_link('Sign out',    href: signout_path) }
   		it { should_not have_link('Sign in', href: signin_path) }
 
@@ -90,6 +91,11 @@ describe "AuthenticationPages" do
         describe "submitting to the update action" do
           before { patch user_path(user) }
           specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_title('Sign in') }
         end
       end
     end

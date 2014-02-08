510e5074e99de7585156880a17a81be4372ffe4a
May Miller-Ricci <maymillerricci@gmail.com>
2014-02-08 11:15:53 -0800

    finish updating users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,10 +17,12 @@ describe User do
 	it { should respond_to(:password) }
 	it { should respond_to(:password_confirmation) }
 	it { should respond_to(:remember_token) }
-	it { should respond_to(:authenticate) }
+	it { should respond_to(:remember_token) }
+	it { should respond_to(:admin) }
 
 	#calls valid? method - user.valid?
 	it { should be_valid }
+	it { should_not be_admin }
 
 	describe "when name is not present" do
 		before { @user.name = " " }
